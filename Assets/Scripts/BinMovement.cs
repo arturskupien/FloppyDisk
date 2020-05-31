5334cfe3452333f2ed21626183f42cd09849f087
arturskupien <arturskupien98@gmail.com>
2020-05-31 18:22:02 +0200

    Scripts

---
+++ Assets/Scripts/BinMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinMovement : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    public Collider2D playerCollider;
+
+    void Update()
+    {
+        rigidbody.AddForce(new Vector3(0.5f,0f,0f));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Score"))
+        {
+            //score++ or some shit
+        }
+        else if(collision.CompareTag("Obsticle"))
+        {
+            //GameOver
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    [SerializeField] float jumpPower;
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(new Vector3(0f,jumpPower,0f));
+
+            Debug.Log("Done");
+        }
+    }
+}
